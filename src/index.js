bb02747044b3fee3858c893b156716a476d7a5fe
aydinugur <aydinugur@yahoo.com>
2022-08-31 21:25:39 +0200

    Add webpack and index files

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import kontra from './kontra.mjs';
+
+let {canvas} = kontra.init();
+
+const sprite = kontra.Sprite({
+    x: 100,        // starting x,y position of the sprite
+    y: 80,
+    color: 'black',  // fill color of the sprite rectangle
+    width: 60,     // width and height of the sprite rectangle
+    height: 120,
+    dx: 2          // move the sprite 2px to the right every frame
+});
+
+var loop = kontra.GameLoop({  // create the main game loop
+    update: function() {        // update the game state
+        sprite.update();
+
+        // wrap the sprites position when it reaches
+        // the edge of the screen
+        if (sprite.x > canvas.width) {
+            sprite.x = -sprite.width;
+        }
+    },
+    render: function() {        // render the game state
+        sprite.render();
+    }
+});
+
+loop.start();
